738e6168f88831b5493a26b898edeb59b10904f7
Aaron Hayes <aaronhayes@users.noreply.github.com>
2019-11-01 21:29:27 +1000

    Fix import orders

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Events, scroller } from 'react-scroll';
+
 import debounce from 'lodash.debounce';
 
 function useScrollSpy(): string | undefined {
